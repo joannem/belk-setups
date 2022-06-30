3cc0296d39c12ec88b85ee0a9e8cbd85adc0bafb
Joanne Mah <joannemah.j.w@gmail.com>
2022-06-30 00:41:15 -0700

    feat: Added Logstash pipeline to ingest from Kafka

--- 2_logstashc/up_logstash_cluster.sh
+++ 2_logstashc/up_logstash_cluster.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo cp /var/lib/docker/volumes/1_elasticc-kibana_certs/_data/ca/ca.crt . && \
+
 docker run -d \
 	--name=logstash \
 	--user=root \
 	--network=elasticc-kibana_default \
-	--volume="$(pwd)/ca.crt:/usr/share/logstash/certs/ca.crt:z" \
+	--volume="$(pwd)/ca.crt:/usr/share/logstash/certs/ca.crt:ro" \
 	--volume="$(pwd)/logstash.docker.yml:/usr/share/logstash/config/logstash.yml:ro" \
 	--volume="$(pwd)/pipeline/:/usr/share/logstash/pipeline/:ro" \
 	docker.elastic.co/logstash/logstash:8.2.3
