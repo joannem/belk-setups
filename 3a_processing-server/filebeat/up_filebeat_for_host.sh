2bcca80ae4c72d5e11e65a7add962aec9deb867d
Joanne Mah <joannemah.j.w@gmail.com>
2022-07-14 00:57:28 -0700

    fix: Typo in folder name

--- 3a_processing-server/filebeat/up_filebeat_for_host.sh
+++ 3a_processing-server/filebeat/up_filebeat_for_host.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+rm -rf procsvr-filebeat-certs 
+mkdir procsvr-filebeat-certs/
+
+sudo cp \
+	/var/lib/docker/volumes/1_elasticc-kibana_certs/_data/ca/ca.crt \
+	procsvr-filebeat-certs/. && \
+../create_ssl_cert_key.sh procsvr-filebeat && \
+
 docker run -d \
 	--name=procsvr-filebeat \
 	--network=monitoring_network \
+	--volume="$(pwd)/procsvr-filebeat-certs:/usr/share/filebeat/certs:ro" \
 	--volume="$(pwd)/procsvr.filebeat.docker.yml:/usr/share/filebeat/filebeat.yml:ro" \
 	--volume="$(pwd)/tutorial-dataset:/data/tutorial-dataset.log:ro" \
-	--volume="1_elasticc-kibana_certs:/etc/pki:ro" \
 	docker.elastic.co/beats/filebeat:8.2.3 filebeat -e --strict.perms=false
-
-# --user=root \
-# --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
-# --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
